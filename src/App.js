a5bab9faaa5a8f6126c6fba487384cf7e706b60f
obourne <obourneo@gmail.com>
2020-02-19 11:49:26 +0000

    remove duplication in button nav

--- src/App.js
+++ src/App.js
@@ -3,31 +3,30 @@ import FormLive from "./FormLive";
 import FormSubmit from "./FormSubmit";
 import FormNoA11y from "./FormNoA11y";
 import "./App.css";
-
+const formsList = [
+  { id: "no-a11y", name: "Poor a11y" },
+  { id: "live", name: "Live validation" },
+  { id: "submit", name: "Submit validation" }
+];
 const App = () => {
   const [form, setForm] = useState("no-a11y");
+
   return (
     <div className="App">
       <h1>A11y Forms Example</h1>
       <nav className="nav-buttons">
-        <button
-          className={form === "no-a11y" ? "button-active" : undefined}
-          onClick={() => setForm("no-a11y")}
-        >
-          Form with bad a11y
-        </button>
-        <button
-          className={form === "live" ? "button-active" : undefined}
-          onClick={() => setForm("live")}
-        >
-          Form with live validation
-        </button>
-        <button
-          className={form === "submit" ? "button-active" : undefined}
-          onClick={() => setForm("submit")}
-        >
-          Form with submit validation
-        </button>
+        {formsList.map(({ id, name }) => {
+          const selected = form === id;
+          return (
+            <button
+              className={selected ? "button-active" : undefined}
+              aria-pressed={selected}
+              onClick={() => setForm(id)}
+            >
+              {name}
+            </button>
+          );
+        })}
       </nav>
 
       {form === "live" ? (
